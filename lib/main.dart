8a4d10a2c09bc5119f05e9accfd7c3a605d0d47e
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2022-12-23 21:44:12 +0200

    add _getBatteryLevel methode to native_code.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'method_channel/native_code.dart';
 
@@ -7,24 +6,14 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  static const platform = MethodChannel('samples.flutter.dev/battery');
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Method Channel',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const NativeCode(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NativeCode(),
     );
   }
 }
--- lib/method_channel/native_code.dart
+++ lib/method_channel/native_code.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class NativeCode extends StatelessWidget {
+class NativeCode extends StatefulWidget {
   const NativeCode({Key? key}) : super(key: key);
 
   @override
+  State<NativeCode> createState() => _NativeCodeState();
+}
+
+class _NativeCodeState extends State<NativeCode> {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+
+  // Get battery level.
+  String _batteryLevel = 'Unknown battery level.';
+
+  _getBatteryLevel() {
+    platform.invokeMethod('getBatteryLevel').then((value) {
+      setState(() {
+        _batteryLevel = 'Battery level at $value % .';
+      });
+    }).catchError((onError) {
+      setState(() {
+        _batteryLevel = "Failed to get battery level: '${onError.message}'.";
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: _getBatteryLevel,
+              child: const Text('Get Battery Level'),
+            ),
+            Text(_batteryLevel),
+          ],
+        ),
+      ),
+    );
   }
 }
